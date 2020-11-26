04918ecc03a2f721ddaf1b316ade14981e2a5b16
Alisher <>
2020-11-26 23:11:39 +0600

    [ADD] добавили тэги и сделели чтобы можно было переходить через него

--- models.py
+++ models.py
@@ -7,7 +7,12 @@ def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s) #тут указывается что меняем переменная патерн на что на дифисы и на патрны мы указали что меняем пробелы потом меняем с помощью локальной переменной что мы передали снизу когда вызывали этот класс
 
-class Post(db.Model):#создаетм таблицу
+post_tags = db.Table('post_tags',
+                    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+    )
+
+class Post(db.Model):#создаетм таблицуs
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     slug = db.Column(db.String(140), unique=True)
@@ -18,9 +23,24 @@ class Post(db.Model):#создаетм таблицу
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
     def generate_slug(self):#генерировать ссылки еогда мы пишем тайтл то то все идет в ссылку но это сылка через слаги фай редактируется убирается пробелы и заменяются дифизами
         if self.title:
             self.slug = slugify(self.title)
 
     def __repr__(self):#чтобы нормально создавалась таблица и можно было понимать его
         return '<Post id:{}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):#чтобы нормально создавалась таблица и можно было понимать его
+        return '<Tag id:{}, name: {}>'.format(self.id, self.name)
